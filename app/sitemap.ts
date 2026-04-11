d1550845f8d209db700897fab7e02d92f31db140
hamka <hamkazainulardhi@gmail.com>
2026-04-11 10:28:01 +0700

    add: google verify2222

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mbg-adminpanel.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mbg-adminpanel.vercel.app/dashboard",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mbg-adminpanel.vercel.app/pendaftaran",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mbg-adminpanel.vercel.app/laporan",
+      lastModified: new Date(),
+    },
+  ];
+}
